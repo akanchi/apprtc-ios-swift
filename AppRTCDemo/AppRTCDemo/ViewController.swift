b26473f61a6a884ed3cf47a85bea0735073c469f
akanchi <chenpx718@gmail.com>
2021-01-08 11:29:00 +0800

    创建工程

---
+++ AppRTCDemo/AppRTCDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppRTCDemo
+//
+//  Created by akanchi on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
